1188af41dd5889a45a657b7b3093c9f8855f7c6b
KH <32192638+WingsMaker@users.noreply.github.com>
2023-12-30 13:57:43 +0800

    Add files via upload

---
+++ ask-palm.js
@@ -0,0 +1,34 @@
+
+function ask_palm(prompt) {
+	let msg;
+	let url;
+	let prompt_text;
+	let requestBody;
+	let payload;
+	let result;
+	msg = "";
+	url = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyCHNsZU2T8NYy_VTfOT1q9Bgow_y-Hp_ss";
+	prompt_text = {
+		'text': prompt
+	}
+	requestBody = {
+		"prompt": prompt_text
+	}
+	payload = {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	}
+	return fetch(url, payload)
+	.then(response => response.json())
+	.then(data => data.candidates[0].output)
+	.then((msg) => {
+		addChat(prompt, msg);
+	})
+	.catch(error => {
+		console.error('Error calling PALM2 API:', error);
+	});
+}
+// explains chatgpt to a 10 years old boy
